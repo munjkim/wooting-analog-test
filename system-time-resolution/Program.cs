4bfe30c2200e251117f04c5f9c5954fe3bccd5d9
Munjeong_Kim <answjd3759@gmail.com>
2024-05-27 13:11:36 +0900

    checking system time resolution

---
+++ system-time-resolution/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine($"Timer frequency in ticks per second = {Stopwatch.Frequency}");
+double resolution = 1.0 / Stopwatch.Frequency;
+Console.WriteLine($"Timer resolution in seconds = {resolution}");

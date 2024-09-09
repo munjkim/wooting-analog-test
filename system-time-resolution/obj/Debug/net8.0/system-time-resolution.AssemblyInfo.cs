3ac186e8c5512ef687ec4fadd9d99acc5f579afd
Munjeong_Kim <answjd3759@gmail.com>
2024-09-10 04:02:38 +0900

    Update assembly version in wooting-analog-test and system-time-resolution

--- system-time-resolution/obj/Debug/net8.0/system-time-resolution.AssemblyInfo.cs
+++ system-time-resolution/obj/Debug/net8.0/system-time-resolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system-time-resolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c1d1c6721e295bef6c9874370dd9eaff7c7289")]
 [assembly: System.Reflection.AssemblyProductAttribute("system-time-resolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system-time-resolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
